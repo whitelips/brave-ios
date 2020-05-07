51390db2221975f5221d1ddc4509747b0d2ba807
jamesmudgett <jmm.seasons@gmail.com>
2020-05-07 10:51:38 -0500

    clear playlistItems when navigating on YT

--- Client/Frontend/Browser/Tab.swift
+++ Client/Frontend/Browser/Tab.swift
@@ -519,6 +519,10 @@ class Tab: NSObject {
             return
         }
 
+        if playlistItems.value.count > 0 {
+            playlistItems.value = []
+        }
+        
         self.urlDidChangeDelegate?.tab(self, urlDidChangeTo: url)
     }
 
